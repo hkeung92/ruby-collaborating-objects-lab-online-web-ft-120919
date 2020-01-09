9cbd03e52c6d9411ca6647f322bb83b80b5edc5b
Ho Yin Keung <hykeung92@gmail.com>
2020-01-09 20:17:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,7 +21,7 @@ class Artist
   end
   
   def self.find_or_create_by_name(name)
-    @@all.find {|artist| artist.name == self}
+    @@all.find {|artist| artist.name == name}
     if self.find(name)
       self.find(name)
     else
